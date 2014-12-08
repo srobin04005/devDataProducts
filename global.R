cd03dc5804880a319f5c61408934f852ae41f85c
Scott Robinett <srobin04005@gmail.com>
2014-12-08 11:00:20 -0500

    Revert "updated"
    This reverts commit 4ad390b0264b8cce349ae375d424830538b6df44.

--- global.R
+++
@@ -1,6 +0,0 @@
-
-dataset <- mtcars
-lm_formula <- lm(mpg~as.numeric(cyl)+hp+wt+drat, data=dataset)
-glm_formula <- glm(mpg~as.numeric(cyl)+hp+wt+drat, data=dataset)
-
-# "mpg"  "cyl"  "disp" "hp"   "drat" "wt"   "qsec" "vs"   "am"   "gear" "carb"
